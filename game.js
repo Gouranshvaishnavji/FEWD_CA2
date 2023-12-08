4740910482f754e5a89a626a6928354d16ab9039
Gouransh <147132368+Gouranshvaishnavji@users.noreply.github.com>
2023-12-08 17:48:08 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,3 @@
-
-// function musicPlay() {
-//     var audio = new Audio('./click3.mp3');
-//     audio.play();
-// }
-// ['itachi.ico', 'Dora.png', 'monkey.png', 'santa.png', 'boots-dora.png']
-// function musicStop() {
-//     var audio = document.getElementById("audio");
-//     audio.pause();
-// }
-// play.addEventListener("onlick", musicPlay)
-// play.addEventListener("click", musicStop)
 document.addEventListener('DOMContentLoaded', function () {
     var images = ['./assests/Dora.png', './assests/monkey.png', './assests/boots-dora.png', './assests/simba.png','./assests/sniper.png'] //image URLs to this array
     var clickSound = new Audio('click3.mp3'); // click sound URL
@@ -22,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
         timeLimit--;
         timerElement.textContent = timeLimit + ' seconds';
     let message;
-
+//for alert message
     if (score < 3) {
         message = 'You score is less than 3 :'; 
     } else {
@@ -31,6 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (timeLimit === 0) {
             clearInterval(timerId);
             alert(message + score);
+            // updating score in local storage
             localStorage.setItem('finalScore', score);
             window.location.href = 'score.html'; // Redirect to score.html
         }
@@ -38,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.addEventListener('click', function (event) {
         if (event.target.tagName === 'IMG') {
-            // If the clicked element is an image
+            // if th clicked element is an imge
             clickSound.play();
             score++;
             scoreElement.textContent = score;
@@ -52,26 +41,18 @@ document.addEventListener('DOMContentLoaded', function () {
         var randomIndex = Math.floor(Math.random() * images.length);
         randomImage.src = images[randomIndex];
 
-        // Set the image position
-        randomImage.style.position = 'absolute';
-        randomImage.style.left = Math.random() * (window.innerWidth - 80) + 'vw';
-        randomImage.style.top = Math.random() * (window.innerHeight - 80) + 'vh';
+        // Setting the image position
+        var maxWidth = window.innerWidth * 0.6; // 60vw
+        var maxHeight = window.innerHeight * 0.7; // 70vh
 
-        // Append the image to the body
+        randomImage.style.position = 'absolute';
+        randomImage.style.left = Math.random() * (maxWidth - 100) + 'px';
+        randomImage.style.top = Math.random() * (maxHeight - 100) + 'px';
+
         document.body.appendChild(randomImage);
     }
     
     // Initial image spawn
     spawnRandomImage();
 });
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Retrieve the score from local storage
-//     var finalScore = localStorage.getItem('finalScore');
 
-//     // Display the score on the page
-//     var finalScoreElement = document.getElementById('finalScore');
-//     finalScoreElement.textContent = 'Score: ' + finalScore;
-
-//     // Clear the score from local storage
-//     localStorage.removeItem('finalScore');
-// });
